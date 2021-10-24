7ad96552f40d1e7f2fbca0d7d7178ca6f90bb8c4
marcellinuselbert11 <marcellinuselbert46@gmail.com>
2021-10-24 23:08:04 +0700

    add Tambah Jadwal button route to Buat Jadwal page on Daftar Jadwal page

--- src/containers/ScheduleList/index.js
+++ src/containers/ScheduleList/index.js
@@ -17,7 +17,7 @@ import Helmet from "react-helmet";
 import { Link } from "react-router-dom";
 import styled, { css } from "styled-components";
 
-import { useHistory } from 'react-router';
+import { useHistory } from "react-router";
 
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
@@ -27,9 +27,9 @@ import { makeAtLeastMs } from "utils/promise";
 import Schedule from "containers/ViewSchedule/Schedule";
 import clipboardImg from "assets/Clipboard.svg";
 import deleteImg from "assets/Delete.svg";
-import { decodeHtmlEntity } from 'utils/string'
+import { decodeHtmlEntity } from "utils/string";
 import EditIcon from "assets/EditSchedule/EditIcon";
-import { BauhausSide } from 'components/Bauhaus';
+import { BauhausSide } from "components/Bauhaus";
 import BauhausMobile from "assets/Beta/bauhaus-sm.svg";
 import BauhausDesktop from "assets/Beta/bauhaus-lg.svg";
 import { SuccessToast } from "components/Toast";
@@ -38,18 +38,18 @@ import getFormattedSchedule from "utils/schedule";
 const ScheduleList = () => {
   const history = useHistory();
   const dispatch = useDispatch();
-  const auth = useSelector(state => state.auth);
-  const isMobile = useSelector(state => state.appState.isMobile);
+  const auth = useSelector((state) => state.auth);
+  const isMobile = useSelector((state) => state.appState.isMobile);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const [selectedId, setSelectedId] = useState('');
+  const [selectedId, setSelectedId] = useState("");
   const [schedules, setSchedules] = useState();
 
   useEffect(() => {
     const fetchSchedules = async () => {
       dispatch(setLoading(true));
       const {
-        data: { user_schedules }
+        data: { user_schedules },
       } = await makeAtLeastMs(getSchedules(auth.userId), 1000);
       setSchedules(user_schedules);
       dispatch(setLoading(false));
@@ -61,46 +61,45 @@ const ScheduleList = () => {
   const performDeleteSchedule = async (userId, scheduleId) => {
     ReactGA.event({
       category: "Hapus Jadwal",
-      action: "Deleted a schedule"
+      action: "Deleted a schedule",
     });
     dispatch(setLoading(true));
     onClose();
     await makeAtLeastMs(deleteSchedule(userId, scheduleId), 1000);
     const {
-      data: { user_schedules }
+      data: { user_schedules },
     } = await makeAtLeastMs(getSchedules(auth.userId), 1000);
     setSchedules(user_schedules);
     dispatch(setLoading(false));
-  }
+  };
 
-  const confirmDeleteSchedule = (scheduleId)  => {
+  const confirmDeleteSchedule = (scheduleId) => {
     performDeleteSchedule(auth.userId, scheduleId);
-  }
+  };
 
   const showAlertCopy = () => {
     ReactGA.event({
       category: "Bagikan Jadwal",
-      action: "Copied a schedule's URL"
+      action: "Copied a schedule's URL",
     });
-    SuccessToast(
-      "Link berhasil disalin!",
-      isMobile
-    );
-  }
+    SuccessToast("Link berhasil disalin!", isMobile);
+  };
 
   const handleClickEditJadwal = (idJadwal) => {
     history.push(`/edit/${idJadwal}`);
-  }
+  };
 
   const convertDate = (date) => {
-    const dateNew = new Date(date)
-    return `${dateNew.getDate()}/${(dateNew.getMonth())+1}/${dateNew.getFullYear()}`
-  }
+    const dateNew = new Date(date);
+    return `${dateNew.getDate()}/${
+      dateNew.getMonth() + 1
+    }/${dateNew.getFullYear()}`;
+  };
 
   const showDialogDelete = (id) => {
     setSelectedId(id);
     onOpen();
-  }
+  };
 
   return (
     <Container>
@@ -128,17 +127,24 @@ const ScheduleList = () => {
         meta={[{ name: "description", content: "Description of Jadwal" }]}
       />
 
-      {schedules && schedules.length > 0? (
+      {schedules && schedules.length > 0 ? (
         <>
           <BauhausSide />
-          <PageTitle mobile={isMobile}>Daftar Jadwal</PageTitle>
-        </>
-      ): ""}
 
+          <PageTitle mobile={isMobile}>Daftar Jadwal</PageTitle>
+          <Link to={`/susun`}>
+            <AddJadwalButton mobile={isMobile}>
+              <Button variant="outline">Tambah Jadwal</Button>
+            </AddJadwalButton>
+          </Link>
+        </>
+      ) : (
+        ""
+      )}
       {schedules && schedules.length > 0 ? (
         <CardContainer>
           {schedules.map((schedule, idx) => {
-            const [, totalCredits] = getFormattedSchedule(schedule)
+            const [, totalCredits] = getFormattedSchedule(schedule);
 
             return (
               <Card key={`${schedule.name}-${idx}`}>
@@ -146,8 +152,8 @@ const ScheduleList = () => {
                   <Link to={`/jadwal/${schedule.id}`}>
                     <h2>{decodeHtmlEntity(schedule.name) || "Untitled"}</h2>
                     <h4>
-                      Dibuat pada {convertDate(schedule.created_at)}
-                      {' '} • {totalCredits} SKS
+                      Dibuat pada {convertDate(schedule.created_at)} •{" "}
+                      {totalCredits} SKS
                     </h4>
                   </Link>
                   <CardActionContainer>
@@ -161,7 +167,10 @@ const ScheduleList = () => {
                       src={deleteImg}
                       onClick={() => showDialogDelete(schedule.id)}
                     />
-                    <EditIcon className="editIcon" onClick={() => handleClickEditJadwal(schedule.id)} />
+                    <EditIcon
+                      className="editIcon"
+                      onClick={() => handleClickEditJadwal(schedule.id)}
+                    />
                   </CardActionContainer>
                 </div>
                 <Schedule
@@ -173,34 +182,48 @@ const ScheduleList = () => {
                   showRoom
                 />
               </Card>
-            )
+            );
           })}
         </CardContainer>
       ) : (
         <>
           {isMobile ? (
-              <AssetBauhaus
-                isMobile={isMobile}
-                src={BauhausMobile}
-                alt="bauhaus-sm"
-              />
+            <AssetBauhaus
+              isMobile={isMobile}
+              src={BauhausMobile}
+              alt="bauhaus-sm"
+            />
           ) : (
             <AssetBauhaus src={BauhausDesktop} alt="bauhaus-lg" />
           )}
-          <Box pt="90px" mb={{base:16,md:'160px'}}>
-            <div style={{  textAlign: isMobile?  "center": "left", width: "100%" }}>
-            <PageTitleNoSchedule mobile={isMobile}>Daftar Jadwal</PageTitleNoSchedule>
-              <PageInfo mobile={isMobile}>Anda belum pernah membuat jadwal. Mulai susun jadwal anda sekarang!</PageInfo>
+          <Box pt="90px" mb={{ base: 16, md: "160px" }}>
+            <div
+              style={{ textAlign: isMobile ? "center" : "left", width: "100%" }}
+            >
+              <PageTitleNoSchedule mobile={isMobile}>
+                Daftar Jadwal
+              </PageTitleNoSchedule>
+              <PageInfo mobile={isMobile}>
+                Anda belum pernah membuat jadwal. Mulai susun jadwal anda
+                sekarang!
+              </PageInfo>
               <Link to={`/susun`}>
-                <Button height={{ base: "44px", md: "57px" }} width={{ base: "137px", md: "194px" }} ml={{md: 12}} fontSize={{ base: "14px", md: "18px" }}>Buat Jadwal</Button>
+                <Button
+                  height={{ base: "44px", md: "57px" }}
+                  width={{ base: "137px", md: "194px" }}
+                  ml={{ md: 12 }}
+                  fontSize={{ base: "14px", md: "18px" }}
+                >
+                  Buat Jadwal
+                </Button>
               </Link>
             </div>
           </Box>
         </>
-        )}
+      )}
     </Container>
   );
-}
+};
 
 const ModalContent = styled(ChakraModalContent).attrs({
   padding: { base: "16px 24px", lg: "20px 24px" },
@@ -220,28 +243,28 @@ const ModalFooter = styled(ChakraModalFooter).attrs({
 `;
 
 const Container = styled.div`
-  margin-left:-3rem;
+  margin-left: -3rem;
   margin-right: -3rem;
 `;
 
 const CardActionContainer = styled.div`
-display:flex;
-flex-direction:'row';
-justify-content: center;
-align-items:center;
+  display: flex;
+  flex-direction: "row";
+  justify-content: center;
+  align-items: center;
 
-.editIcon{
-  margin-left:8px;
-  cursor:pointer;
-}
+  .editIcon {
+    margin-left: 8px;
+    cursor: pointer;
+  }
 `;
 
 const PageTitle = styled.h1`
-  margin: ${({ mobile }) => (mobile ? "-40px 0 0 0px" : "0px 48px 30px 48px")};
+  margin: ${({ mobile }) => (mobile ? "-40px 0 0 0px" : "0px 48px 12px 48px")};
   font-size: ${({ mobile }) => (mobile ? "1.7rem" : "2rem")};
   text-align: center;
   font-weight: bold;
-  color: #5038BC;
+  color: #5038bc;
 
   @media (min-width: 900px) {
     text-align: left;
@@ -251,10 +274,15 @@ const PageTitle = styled.h1`
 const PageTitleNoSchedule = styled.h1`
   font-size: ${({ mobile }) => (mobile ? "50px" : "64px")};
   font-weight: bold;
-  color: #5038BC;
+  color: #5038bc;
   margin: ${({ mobile }) => (mobile ? "2rem" : "32px 48px 16px 48px")};
 `;
-
+const AddJadwalButton = styled.div`
+  margin: ${({ mobile }) => (mobile ? "20px 0 10px" : "0px 48px 20px 48px")};
+  display: ${({ mobile }) => (mobile ? "flex" : "static")};
+  justify-content: center;
+}}
+`;
 const PageInfo = styled.h2`
   font-size: ${({ mobile }) => (mobile ? "14px" : "18px")};
   margin: ${({ mobile }) => (mobile ? "2rem" : "32px 48px 48px 48px")};
@@ -262,14 +290,14 @@ const PageInfo = styled.h2`
 `;
 
 const Card = styled.div`
-  border: 0.05px solid #E5E5E5;
+  border: 0.05px solid #e5e5e5;
   border-radius: 8px;
   h2 {
     color: #333333;
     font-weight: bold;
     font-size: 18px;
   }
-  h4{
+  h4 {
     color: #333333;
     font-size: 12px;
   }
@@ -278,10 +306,10 @@ const Card = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: #F5F5F5;
+    background-color: #f5f5f5;
     border-radius: 8px 8px 0 0;
   }
-  ${props =>
+  ${(props) =>
     !props.theme.mobile &&
     css`
       width: 49%;
@@ -305,8 +333,8 @@ const CardContainer = styled.div`
   display: flex;
   width: 100%;
   flex-wrap: wrap;
-  flex-direction: ${props => (props.theme.mobile ? "column" : "row")};
-  padding: ${props => (props.theme.mobile ? "1rem 3rem 0 3rem" : "0 48px")};
+  flex-direction: ${(props) => (props.theme.mobile ? "column" : "row")};
+  padding: ${(props) => (props.theme.mobile ? "1rem 3rem 0 3rem" : "0 48px")};
   background-color: #ffffff;
 `;
 
@@ -327,8 +355,9 @@ const AssetBauhaus = styled.img`
   right: 0;
   top: 0;
 
-
-  ${props => props.isMobile && (`
+  ${(props) =>
+    props.isMobile &&
+    `
     top: 50px;
     width: 100%;
     display: block;
@@ -336,7 +365,7 @@ const AssetBauhaus = styled.img`
     @media (min-width: 540px) {
       display: none;
     }
-  `)}
+  `}
 `;
 
 export default ScheduleList;
